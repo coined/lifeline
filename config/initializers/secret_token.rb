014da5d70a0f7c82495f444ba1cb39be232b4253
Colin Anderson <colin.anderson@gmail.com>
2011-12-04 20:40:26 -0800

    Adding base rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifeline::Application.config.secret_token = '66983add69e3704139fb3cc18236f1baa37eb14f56e83fc13b195376a54b3a44fe4ced13d43bdf6f639fc0c2353daa39949ab21b82c390a8c338d49717ddb111'
